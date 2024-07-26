5f8b5a5a8619a0ed2f27456db5f15d2c9ffd1886
Jeff Cousino <jcousin3@gmail.com>
2024-07-26 00:35:58 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { MantineProvider } from "@mantine/core";
+
+function App() {
+
+  return (
+    <MantineProvider defaultColorScheme="auto">
+
+    </MantineProvider>
+  )
+}
+
+export default App
